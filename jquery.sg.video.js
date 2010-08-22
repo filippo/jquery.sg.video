a87a41ed701fddd1a7637ce40fba3641546d8a9a
Filippo Pacini <filippo.pacini@gmail.com>
2010-08-22 23:35:54 +0200

    rendering the video tag. Improve using jquery dom manipulation features

--- jquery.sg.video.js
+++ jquery.sg.video.js
@@ -1,8 +1,10 @@
 (function($){
      var settings = {'preferredTag':    'video',
-		     'extensions': {'ogg':   '.ogg',
-				    'h264':  '.avi',
-				    'webm':  '.wbm',
+		     'autoplay': false,
+		     'controls': true,
+		     'extensions': {'ogg':   '.ogv',
+				    'h264':  '.mp4',
+				    'webm':  '.webm',
 				    'flash': '.swf'
 				   }
 		    };
@@ -26,11 +28,33 @@
      // extend $.support with video informations
      $.extend($.support, {'video': videoSupport});
 
+     var videoTag = function(src, options) {
+	 var str = "<video src=\""+src;
+	 if ($.support['video']['webm'] !== '') {
+	     str += options['extensions']['webm'];
+	 } else if ($.support['video']['h264'] !== '') {
+	     str += options['extensions']['h264'];
+	 } else {
+	     str += options['extensions']['ogg'];
+	 }
+	 str += '" ';
+	 if (options['controls']) {
+	     str += 'controls="controls" ';
+	 }
+	 if (options['autoplay']) {
+	     str += 'autoplay="autoplay" ';
+	 }
+	 str += '>your browser does not support the video tag</video>';
+	 return str;
+     };
+
      // put the video object inside the the wrapped set
      $.fn.video = function(videoBaseName, callerSettings) {
 	 settings = $.extend(settings, callerSettings||{});
 	 settings['video'] = videoBaseName;
-	 this.html('here is the video: '+settings['video']);
+	 if (settings['preferredTag'] == 'video' && videoSupport) {
+	     this.html(videoTag(videoBaseName, settings));
+	 }
 	 return this;
      };
 })(jQuery);
